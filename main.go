c06a676f02277313d4ecfc280b9abd090a3a81d6
chotepurespirit <chovyzzz.nai@gmail.com>
2024-04-25 18:56:48 +0700

    learn about my array with go part2

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import "fmt"
 
 
 func main() {
-	var myArray [3]int // Declare an array of 3 integers
+	var myArray [5]int // Declare an array of 3 integers
 	myArray[0] = 10 // Assign the first element a value of 10
 	myArray[1] = 15 // Assign the second element a value of 15
 	myArray[2] = 39 // Assign the third element a value of 39
-	fmt.Printf("My array is : %d\n", myArray)
-	fmt.Println("My Array !! >> ", myArray)
+	myArray[3] = 25 // Assign the fourth element a value of 25
+	myArray[4] = 50 // Assign the fifth element a value of 50
+
+	for i :=0 ; i < len(myArray) ; i++ {
+		fmt.Println(myArray[i])
+		fmt.Printf("my size array is %d\n", len(myArray))
+	}
 }
