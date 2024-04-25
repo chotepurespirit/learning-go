b4c49527572b415edf58ee48815c739bc74f8f7b
chotepurespirit <chovyzzz.nai@gmail.com>
2024-04-25 18:47:20 +0700

    learn about my array with go

---
+++ chote/choteloveearn.go
@@ -0,0 +1,12 @@
+package choteloveearn
+
+import "fmt"
+
+func sayLove() {
+	fmt.Println("I love you, Earn")
+}
+
+func SayChote() {
+	fmt.Println("Hello, Chote")
+	sayLove()
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var myArray [3]int // Declare an array of 3 integers
+	myArray[0] = 10 // Assign the first element a value of 10
+	myArray[1] = 15 // Assign the second element a value of 15
+	myArray[2] = 39 // Assign the third element a value of 39
+	fmt.Printf("My array is : %d\n", myArray)
+	fmt.Println("My Array !! >> ", myArray)
+}
